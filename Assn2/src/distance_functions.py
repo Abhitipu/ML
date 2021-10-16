c12e327a6f2f312118ba731528287edc9c456329
Abhitipu <abhinandan0316@gmail.com>
2021-10-16 13:03:48 +0530

    Optimized distance functions

--- Assn2/src/distance_functions.py
+++ Assn2/src/distance_functions.py
@@ -2,15 +2,29 @@
 import numpy as np
 
 def cosine_similarity(X, Y):
-    X_mag = np.sqrt(sum([i**2 for i in X]))
-    Y_mag = np.sqrt(sum([i**2 for i in Y]))
-    dot_prod = sum([x*y for x, y in X, Y])
-    return dot_prod / (X_mag * Y_mag)
+    if(np.linalg.norm(X) == 0):
+        print("Nooooooooo")
+    if(np.linalg.norm(Y) == 0):
+        print("Nooooooooo")
+    unit_X = X / np.linalg.norm(X)
+    unit_Y = Y / np.linalg.norm(Y)
+    return np.dot(unit_X, unit_Y)
 
 def manhattan_distance(X, Y):
-    ans = sum([np.abs(x - y) for x, y in X, Y])
-    return ans
+    ans = np.abs(X - Y)
+    return np.sum(ans)
 
 def euclidian_distance(X, Y):
-    ans = np.sqrt(sum([(x - y)**2 for x, y in X, Y]))
-    return ans
+    ans = np.square(X - Y)
+    return np.sqrt(np.sum(ans))
+
+if __name__ == "__main__":
+    a = np.random.rand(10)
+    b = np.random.rand(10)
+
+    print(a)
+    print(b)
+
+    print(cosine_similarity(a, b))
+    print(manhattan_distance(a, b))
+    print(euclidian_distance(a, b))
