225575d34a13ad939344b348fc29248e1d5ad36c
Abhitipu <abhinandan0316@gmail.com>
2021-10-17 10:11:54 +0530

    Added comments in model

--- Assn2/src/knn_model.py
+++ Assn2/src/knn_model.py
@@ -1,9 +1,8 @@
 '''
-this is the classifier to be used
-first initialize with all the required data
-then class predict
-after that, call compute_accuracy
-done
+This is the classifier model. 
+First we initialize with the dataset.
+Then we find the nearest neighbors
+After that we make predictions by varying k (no of neighbors)
 '''
 
 import numpy as np
@@ -11,11 +10,13 @@ import numpy as np
 class knn_classifier:
     '''
         This knn classifier constructs a classification for all possible k
-        May change later
     '''
     def __init__(self, X_train, X_test, y_train, y_test, distance_function):
+        '''
+            First we initialize with the dataset and update the distance function
+        '''
         
-        self.X_train = X_train            # converting to d dimensional matrices
+        self.X_train = X_train            
         self.X_test = X_test
         self.y_train = y_train
         self.y_test = y_test
@@ -25,26 +26,29 @@ class knn_classifier:
 
     def predict(self):
         '''
-            Computes an array of predictions of X_test
+            Here, we compute a 2-d array of predictions for all instances in X_test
+            Then we vary the number of neighbors and predict the class value
+            This leads to a better time complexity
         '''
-        self.predictions = []
+        self.predictions = []           # 2d array of predictions
+
         idx2 = 0
         for new_point in self.X_test:
             neighbors = []
             curr_predictions = []
             idx = 0
             
+            # Obtain the indices and the distances from new_point
             indices = np.arange(len(self.X_train))
             distances = np.array([self.distance_function(new_point, given_point) for given_point in self.X_train])
-
-            neighbors = np.column_stack((distances, indices))
             
+            neighbors = np.column_stack((distances, indices))
             neighbors = neighbors[neighbors[:,0].argsort()]     # sorting by distances
 
             spam_labels = 0
             ham_labels = 0
 
-            # so here we will find the values by varying the number of neighbors from 1 to len(X_train)
+            # Here we will find the values by varying the number of neighbors from 1 to len(X_train)
 
             for k in range(len(neighbors)):
                 index = int(neighbors[k][1])
@@ -57,6 +61,8 @@ class knn_classifier:
                 if k == 0:
                     continue
 
+                # We just check the greater frequency
+                # and classify accordingly
                 if spam_labels > ham_labels:
                     curr_predictions.append(1)
                 else:
@@ -64,7 +70,7 @@ class knn_classifier:
 
             self.predictions.append(curr_predictions)
 
-            if idx2 % 100 == 0:
+            if idx2 % 100 == 0:                             # Printing status after processing every 100 test samples
                 print(f"Done for {idx2 + 1} test samples")
 
             idx2 += 1
@@ -73,19 +79,23 @@ class knn_classifier:
     
     def compute_accuracy(self):
         '''
-            This will have to compute accuracy
-            for variable no of neighbors
-            i.e. from 1 to len(X_train)
+            Here we compute accuracies by varying no of neighbors from 1 to len(X_train)
+            After that we return a list containing the accuracies and the corresponding number of neighbors considered
         '''
         accuracies = []
         num_nbrs = []
 
         for k in range(len(self.y_train) - 1):
+            # Get all the predictions for the given k
             curr_predictions = np.array([predicted_value[k] for predicted_value in self.predictions])
+
+            # Obtain accuracy
             cnt = 0
             for i in range(len(self.y_test)):
                 if self.y_test[i] == curr_predictions[i]:
                     cnt += 1
+
+            # Append to the accuracy list for plotting
             accuracies.append(cnt / len(self.y_test))
             num_nbrs.append(k + 1)
 
