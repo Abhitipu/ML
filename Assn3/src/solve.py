5936858550a2504aca9030275905d4a42d13074a
Abhitipu <abhinandan0316@gmail.com>
2021-11-08 00:30:45 +0530

    Done back prop and check accuracy

--- Assn3/src/solve.py
+++ Assn3/src/solve.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     test_loader = DataLoader(test_dataset, batch_size=1, shuffle=True)
 
     # Set the device
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+    my_device = "cuda" if torch.cuda.is_available() else "cpu"
 
     # Define the hyperparameters
     input_size = 36
@@ -34,5 +34,43 @@ if __name__ == "__main__":
         curr_network = Network(input_size=input_size, output_size=output_size, hidden_layers=hidden_layers)
         print(curr_network)
         
-    # n_epochs = 100
-    # for epoch in range(n_epochs):
+        for learning_rate in required_learning_rates:       
+            optimizer = torch.optim.SGD(curr_network.parameters(), lr=learning_rate)
+            loss_function = torch.nn.MSELoss()
+
+            n_epochs = 100
+            for epoch in range(n_epochs):
+                for batch_idx, (attr_vals, target_values) in enumerate(training_loader):
+                    
+                    attr_vals = attr_vals.to(device=my_device)
+                    target_values = target_values.to(device=my_device)
+
+                    # Forward pass
+                    output = curr_network(attr_vals)
+                    loss = loss_function(output, target_values)
+
+                    # Backward pass
+                    optimizer.zero_grad()
+                    loss.backward()
+
+                    # Update the weights
+                    optimizer.step()
+            
+            def check_accuracy():
+                n_correct = 0
+                n_total = 0
+
+                curr_network.eval()
+
+                # No gradients to be computed for accuracy
+                with torch.no_grad():
+                    for attr_vals, target_values in test_loader:
+                        attr_vals = attr_vals.to(device=my_device)
+                        target_values = target_values.to(device=my_device)
+
+                        output = curr_network(attr_vals)
+                        _, predicted_values = torch.max(output, 1)
+                        n_correct += (predicted_values == target_values).sum().item()
+                        n_total += len(target_values)
+                
+                return n_correct / n_total
